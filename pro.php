55e4f116bd7680e949496be90d214fd0ae78521c
mosama877 <136815539+mosama877@users.noreply.github.com>
2023-12-05 11:54:53 -0800

    problem solving

---
+++ pro.php
@@ -0,0 +1,194 @@
+<?php
+// for ($i = 1; $i <= 10; $i++) {
+//     if ($i == 1) {
+//         echo $i;
+//     } else {
+//         echo "-" . $i;
+//     }
+// }
+
+
+
+
+
+
+
+// $numbers = [1, 1, 1, 2, 2, 3, 6, 7, 7, 4, 5, 5];
+// $unique = array_unique($numbers);
+// // echo print_r($unique) . "<br>" ;
+// sort($unique);
+// print_r($unique);
+
+
+
+
+
+
+
+
+// $str = "eraasoft";
+// $count = substr_count($str, "r");
+// echo $count;
+
+
+
+
+
+
+
+
+// for ($i = 1; $i <= 50; $i++) {
+//     if ($i % 3 == 0 && $i % 5 == 0) {
+//         echo "FizzBuzz " . "<br>";
+//     }
+//     elseif ($i% 3 == 0) {
+//         echo "Fizz ". "<br>";
+//     } 
+//     elseif ($i% 5 == 0) {
+//         echo "Buzz ". "<br>";
+//     } 
+//     else {
+//         echo $i . " ". "<br>";
+//     }
+// }
+
+
+
+
+
+
+// $total = 0;
+// for ($i = 0; $i <= 30; $i++) {
+//     $total += $i;
+// }
+// echo "TOTAL:" . $total;
+
+
+
+
+
+
+
+// $nums = [5, 15, -10, 100, 250, 0, 1];
+// echo max($nums);
+
+
+
+
+
+// $nums = [5, 15, -10, 100, 250, 0, 1];
+// echo min($nums);
+
+
+
+
+
+
+
+// function fac($num){
+//     $xes = 1;
+//     for ($i = 1; $i <= $num; $i++) {
+//         $xes *= $i;
+//     }
+//     return $xes;
+// }
+
+// echo fac(4);
+
+
+
+
+
+
+
+
+
+// $i = 1;
+// while ($i <= 15) {
+//     if ($i % 2 != 0) {
+//         echo $i . "<br>";
+//     }
+//     $i++;
+// }
+
+
+
+
+
+
+
+// $i = 1;
+// while ($i <= 15) {
+//     if ($i%2 == 0) {
+//         echo $i . "<br>";
+//     }
+//     $i++;
+// }
+
+
+
+
+
+
+
+
+
+// $person = array("name" => "John", "age" => 30, "city" => "New York");
+// foreach ($person as $key => $value) {
+//     echo $key . " : " . $value . "(*~*)" . "<br>";
+// }
+
+
+
+
+
+
+
+
+
+// $sales = array("Jan" => 100, "Feb" => 200, "Mar" => 150);
+// $total = 0;
+// foreach ($sales as $value) {
+//     $total += $value;
+// }
+// echo "توتال" . ":" . $total;
+
+
+
+
+
+
+// for ($i = 1; $i <= 10; $i++) {
+//     $esa8 = 8 * $i;
+//     echo "8 x " . $i . " = " . $esa8 . "<br>";
+// }
+
+
+
+
+
+// $students = array(
+//     array("name" => "John", "age" => 20, "grade" => "A"),
+//     array("name" => "Mary", "age" => 22, "grade" => "B"),
+//     array("name" => "Tom", "age" => 18, "grade" => "A")
+// );
+// foreach ($students as $student) {
+//     foreach ($student as $key => $value) {
+//         echo $key . " : " . $value . "<br>";
+//     }
+//     echo "<br>" ; 
+// }
+
+
+
+
+
+
+// $i = 1;
+// do {
+//     if ($i % 3 == 0) {
+//         echo $i . "<br>";
+//     }
+//     $i++;
+// } while ($i <= 100);
+
